b505b51680fc6d074023793543e90b90bc7deb5d
vova <morgan90039@gmail.com>
2021-07-14 00:56:16 +0300

    Добавьте файлы проекта.

---
+++ Countries/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace Countries
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Code = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.capital = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.area = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.population = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.region = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(304, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(244, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.name,
+            this.Code,
+            this.capital,
+            this.area,
+            this.population,
+            this.region});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 100);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(683, 275);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // name
+            // 
+            this.name.HeaderText = "Название";
+            this.name.MinimumWidth = 6;
+            this.name.Name = "name";
+            this.name.ReadOnly = true;
+            this.name.Width = 125;
+            // 
+            // Code
+            // 
+            this.Code.HeaderText = "Код страны";
+            this.Code.MinimumWidth = 6;
+            this.Code.Name = "Code";
+            this.Code.ReadOnly = true;
+            this.Code.Width = 125;
+            // 
+            // capital
+            // 
+            this.capital.HeaderText = "Столица";
+            this.capital.MinimumWidth = 6;
+            this.capital.Name = "capital";
+            this.capital.ReadOnly = true;
+            this.capital.Width = 125;
+            // 
+            // area
+            // 
+            this.area.HeaderText = "Площадь";
+            this.area.MinimumWidth = 6;
+            this.area.Name = "area";
+            this.area.ReadOnly = true;
+            this.area.Width = 125;
+            // 
+            // population
+            // 
+            this.population.HeaderText = "Население";
+            this.population.MinimumWidth = 6;
+            this.population.Name = "population";
+            this.population.ReadOnly = true;
+            this.population.Width = 125;
+            // 
+            // region
+            // 
+            this.region.HeaderText = "Регион";
+            this.region.MinimumWidth = 6;
+            this.region.Name = "region";
+            this.region.ReadOnly = true;
+            this.region.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Code;
+        private System.Windows.Forms.DataGridViewTextBoxColumn capital;
+        private System.Windows.Forms.DataGridViewTextBoxColumn area;
+        private System.Windows.Forms.DataGridViewTextBoxColumn population;
+        private System.Windows.Forms.DataGridViewTextBoxColumn region;
+    }
+}
+
---
+++ Countries/Form1.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Countries
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str;
+            WebRequest req = WebRequest.Create(@"https://restcountries.eu/rest/v2/name/" + textBox1.Text);
+
+            WebResponse response = req.GetResponse();
+            using (Stream s = response.GetResponseStream())
+            {
+                using (StreamReader r = new StreamReader(s))
+                {
+                    str = r.ReadToEnd();
+                }
+            }
+            response.Close();
+            //var weather = JsonConvert.DeserializeObject<Rootobject>(str);
+            var data = JsonConvert.DeserializeObject<Country>(str.Substring(1, str.Length - 2));
+            dataGridView1.Rows.Clear();
+            dataGridView1.Rows.Add();
+            dataGridView1.Rows[0].Cells[0].Value = data.name;
+            dataGridView1.Rows[0].Cells[1].Value = data.alpha2Code;
+            dataGridView1.Rows[0].Cells[2].Value = data.capital;
+            dataGridView1.Rows[0].Cells[3].Value = data.area;
+            dataGridView1.Rows[0].Cells[4].Value = data.population;
+            dataGridView1.Rows[0].Cells[5].Value = data.region;
+
+            //PropertyDescriptorCollection props =
+            //TypeDescriptor.GetProperties(typeof(Country));
+            //DataTable dt = new DataTable();
+            //foreach (PropertyDescriptor p in props)
+            //    dt.Columns.Add(p.Name, p.PropertyType);
+            //object[] value = new object(props[i].GetValue);
+            //dt.Rows.Add();
+            ////DataTable dt = new DataTable();
+            //DataRow dr = dt.NewRow();
+            //dr[]
+            //dt.Row.Add();
+            //Debug.WriteLine(str);
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
